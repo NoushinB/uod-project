017a1ceff13c89decd244560e20c13d3ee7a0d3f
Noushin <noushin.b@icloud.com>
2022-03-07 22:49:11 +0300

    Project initiated

---
+++ android/app/src/main/kotlin/com/example/uod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Uod());
+}
+
+class Uod extends StatelessWidget {
+  const Uod({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
