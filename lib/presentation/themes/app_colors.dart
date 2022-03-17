a3276e97229f9d6aa9635db3083d4aeb70434cc7
Noushin <noushin.b@icloud.com>
2022-03-17 14:29:29 +0300

    Creat App colors & App Assets

---
+++ lib/presentation/themes/app_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColors{
+  // Theme
+  static const Color primaryColor = Color(0xFF005BAA);
+  static const Color lightPrimaryColor = Color(0xFFDFEBFC);
+
+  static const Color secondaryColor = Color(0xF3FFD700);
+  static const Color errorColor = Color(0xFFFB0000);
+
+  // Common
+  static final Color black = Color(0xff050505);
+  static final Color gray = Color(0xff65676B);
+  static final Color lightGray = Color(0xffE4E6EB);
+  static final Color white = Color(0xffFFFFFF);
+}
