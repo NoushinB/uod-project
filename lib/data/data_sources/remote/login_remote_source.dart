4ed04a3f340dd07285c275af3677e5e29d72ffad
Noushin <noushin.b@icloud.com>
2022-03-24 23:38:41 +0300

    Create LoginRemoteSource

---
+++ lib/data/data_sources/remote/login_remote_source.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:uod/core/core.dart';
+import 'package:uod/data/models/token_model.dart';
+
+abstract class LoginRemoteSource {
+  Future<TokenModel> getToken(String userName, String password);
+}
+
+class LoginRemoteSourceImpl implements LoginRemoteSource {
+  final Dio dio;
+
+  const LoginRemoteSourceImpl({required this.dio});
+
+  @override
+  Future<TokenModel> getToken(String userName, String password) async {
+    try {
+      Map<String, dynamic> body = {
+        "grant_type": "password",
+        "client_id": "mobile.admin",
+        "client_secret": "secret",
+        "username": userName,
+        "password": password,
+      };
+      Map<String, dynamic> headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+      };
+      var response = await dio.post(NetworkPath.token, data: body, options: Options(headers: headers));
+      ErrorHelper.ensureSuccessResponse(response, defaultMsg: "");
+      return TokenModel.fromJson(response.data);
+    } catch (ex) {
+      throw ServerException();
+    }
+  }
+}
