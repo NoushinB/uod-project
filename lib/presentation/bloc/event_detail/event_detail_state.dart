936177f36159d0df987f82f1a07a95778d64f60a
Noushin <noushin.b@icloud.com>
2022-06-01 08:40:12 +0300

    first step compleated

--- lib/presentation/bloc/event_detail/event_detail_state.dart
+++ lib/presentation/bloc/event_detail/event_detail_state.dart
@@ -3,17 +3,21 @@ import 'package:uod/core/core.dart';
 import 'package:uod/core/utils/enums/bloc_status.dart';
 import 'package:uod/domain/entities/event_detail.dart';
 
- class EventDetailState extends Equatable{
- final BlocStatus blocStatus;
- final Failure? failure;
- final EventDetail? eventDetail;
+class EventDetailState extends Equatable {
+  final BlocStatus blocStatus;
+  final Failure? failure;
+  final EventDetail? eventDetail;
 
-   const EventDetailState({required this.blocStatus, this.failure,this.eventDetail});
+  const EventDetailState({required this.blocStatus, this.failure, this.eventDetail});
 
- @override
- List<Object?> get props => [blocStatus, failure,eventDetail];
+  @override
+  List<Object?> get props => [blocStatus, failure, eventDetail];
 
- EventDetailState copyWith({BlocStatus? newBlocStatus,Failure? newFailure, EventDetail? newEventDetail}){
-   return EventDetailState(blocStatus: newBlocStatus??blocStatus,failure: newFailure?? failure, eventDetail: newEventDetail??eventDetail);
- }
+  EventDetailState copyWith({BlocStatus? newBlocStatus, Failure? newFailure, EventDetail? newEventDetail}) {
+    return EventDetailState(
+      blocStatus: newBlocStatus ?? blocStatus,
+      failure: newFailure ?? failure,
+      eventDetail: newEventDetail ?? eventDetail,
+    );
+  }
 }
